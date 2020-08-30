d258d9399f025aa3430c647a6c4851b79a954891
Rhythm Paliwal <paliwalrhytem@gmail.com>
2020-08-30 22:32:08 +0530

    third commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 var mongodb = require('mongodb');
+var mongooseTtl = require("mongoose-ttl");
 mongoose.connect('mongodb://localhost/Zomentum',{
 	useNewUrlParser: true,
 	useUnifiedTopology: true
@@ -13,8 +14,8 @@ var userSchema = new mongoose.Schema({
 	name: String,
 	phone: String,
 	timing: Number
-});
-
+},{timestamp: true});
+userSchema.index({createdAt: 1},{expireAfterSeconds: 28800});
 var User = mongoose.model("user",userSchema);
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -40,24 +41,51 @@ app.post("/movie-ticket",function(req,res){
 	var phone = req.body.phone;
 	var timing = req.body.timing;
 	var newUser = {name:name, phone:phone, timing:timing};
-	User.create(newUser,function(err, newlyCreated){
+	User.find({timing: timing}, function(err, docs){
 		if(err){
 			console.log(err);
 		}
 		else{
-			res.redirect("/movie-ticket");
+			console.log(Object.keys(docs).length);
 		}
+		if(Number(Object.keys(docs).length)<20){
+		User.create(newUser,function(err, newlyCreated){
+			if(err){
+				console.log(err);
+			}
+			else{
+				res.redirect("/movie-ticket");
+			}
+		});
+		}
+		else{
+			res.send("Slot not available!");
+		}	
 	});
 });
 
 app.put("/movie-ticket/:id",function(req, res, next){ 
-    User.findByIdAndUpdate(req.params.id, req.body , {new: true}, 
-    	function (err, docs) { 
-    	if (err){ 
-        	return next(err);
-    	} 
-    	res.json(docs);
-}); 
+	User.find({timing: req.body.timing}, function(err, docs){
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log(Object.keys(docs).length);
+		}
+		console.log(Object.keys(docs).length);
+		if(Number(Object.keys(docs).length)<20){
+			User.findByIdAndUpdate(req.params.id, req.body , {new: true}, function (err, docs) { 
+    			if (err){ 
+        			return next(err);
+    			} 
+    			res.json(docs);
+			}); 
+		}
+		else{
+			res.send("Slot not available!");
+		}	
+	});
+    
 });
 
 app.delete("/movie-ticket/:id",function(req, res, next){
@@ -68,9 +96,21 @@ app.delete("/movie-ticket/:id",function(req, res, next){
     } 
     else{ 
         console.log("Removed User : ", docs); 
-        res.send("Deletion Successful"!);
+        res.send("Deletion Successful");
     } 
 }); 
+});
+
+app.get("/movie-ticket/:timing",function(req, res){
+	User.find({timing: req.params.timing}, function(err, docs){
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log(Object.keys(docs).length);
+			res.json(docs);
+		}
+	});
 });
 
 app.listen(3000, function(){
